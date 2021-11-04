abe4b5dd4703731ac1c01fd81135c424ffb70b9f
MarvDan <89084080+MarvDan@users.noreply.github.com>
2021-11-04 17:30:32 +0100

    Update generatePNGfromHTML.js

--- generatePNGfromHTML.js
+++ generatePNGfromHTML.js
@@ -7,7 +7,7 @@ const image = fs.readFileSync('./hexagonal-as-onion.png');
 const base64Image = new Buffer.from(image).toString('base64');
 const dataURI = 'data:image/png;base64,' + base64Image
 
-
+async function selectChangedFiles() {
 let pr = process.env.PR_NUMBER;
 let arr = [];
 try {
@@ -25,7 +25,7 @@ try {
     } catch(e) {
       throw e;
 }
-
+}
 nodeHtmlToImage({
   output: './TEST.png',
   html: '<html><body><img src="{{imageSource}}" /></body></html>',
