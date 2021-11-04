500cb1ee63c0213aa8593c3f722056d8a7ff815c
MarvDan <89084080+MarvDan@users.noreply.github.com>
2021-11-04 15:43:10 +0100

    Update generatePNGfromHTML.js

--- generatePNGfromHTML.js
+++ generatePNGfromHTML.js
@@ -1,12 +1,33 @@
 const nodeHtmlToImage = require('node-html-to-image')
 const fs = require('fs');
+const { Octokit } = require("@octokit/core");
+const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
 
-const image = fs.readFileSync('./image.jpg');
+const image = fs.readFileSync('./hexagonal-as-onion.png');
 const base64Image = new Buffer.from(image).toString('base64');
-const dataURI = 'data:image/jpeg;base64,' + base64Image
+const dataURI = 'data:image/png;base64,' + base64Image
+/*
 
+let pr = process.env.PR_NUMBER;
+let arr = [];
+try {
+      let get = await octokit.request('GET /target/generated-docs/website/pages/architectures/solutions/{solution}', {
+            solution: process.env.PR_SOLUTION,
+            pull_number: pr
+        });
+  
+       let files = get.data;
+  
+       files.forEach(file => {
+            arr.push(file.filename)
+        });
+  
+    } catch(e) {
+      throw e;
+}
+*/
 nodeHtmlToImage({
-  output: './image.png',
+  output: './TEST.png',
   html: '<html><body><img src="{{imageSource}}" /></body></html>',
   content: { imageSource: dataURI }
 })
