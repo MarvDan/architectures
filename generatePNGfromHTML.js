7cc08858f2bfd5025751746d6141cb41a5103085
MarvDan <89084080+MarvDan@users.noreply.github.com>
2021-11-10 16:04:10 +0100

    Update generatePNGfromHTML.js

--- generatePNGfromHTML.js
+++ generatePNGfromHTML.js
@@ -14,13 +14,13 @@ async function selectChangedFiles() {
   core.setOutput('files', files);
 }
 //const image = fs.readFileSync('./image.jpg');
-const base64Image = new Buffer.from(image).toString('base64');
-const dataURI = 'data:image/jpeg;base64,' + base64Image
+//const base64Image = new Buffer.from(image).toString('base64');
+//const dataURI = 'data:image/jpeg;base64,' + base64Image
 
 nodeHtmlToImage({
  // output: './image.png',
-  html: '<html><body><img src="{{imageSource}}" /></body></html>',
-  content: { imageSource: dataURI }
+  //html: '<html><body><img src="{{imageSource}}" /></body></html>',
+  //content: { imageSource: dataURI }
 })
 
 
